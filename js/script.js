0e9eaed5faa7c9b4b1bf3532595eddaca00456ba
AdePhil <adebisi.ahmed10@gmail.com>
2017-07-28 02:59:14 +0100

    adds star Rating

--- js/script.js
+++ js/script.js
@@ -2,12 +2,13 @@ new Vue({
   el:"#app",
   data:{
     links: JSON.parse(localStorage.getItem('links')) || [],
-    text:""
+    text:"",
+    starIncrement:20
   },
   methods:{
     populateLinks:function(e){
        e.preventDefault();
-       const that = this;
+
         const link = {
           text:this.text,
           timesClicked: 0
@@ -22,7 +23,7 @@ new Vue({
       a.href = (this.links[index].text.includes("://")) ? this.links[index].text : "http://" + (this.links[index].text).trim();
       //count times clicked
       this.links[index].timesClicked += 1;
-      //starRating(links,index, starIncrement);
+      this.colorStar(index);
       localStorage.setItem('links', JSON.stringify(this.links));
       a.click();
     },
@@ -30,6 +31,18 @@ new Vue({
       this.links.splice(index,1);
       localStorage.setItem('links', JSON.stringify(this.links));
       console.log(this.links);
+    },
+    deleteAll: function(){
+      this.links = [];
+      localStorage.removeItem('links');
+
+    },
+    colorStar: function(index){
+      const starPer = (this.links[index].timesClicked * this.starIncrement >= 100) ? 100 : this.links[index].timesClicked * this.starIncrement;
+      console.log(starPer);
+      return{
+        width: starPer +'% !important'
+      }
     }
 
   },
